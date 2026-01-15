d5a6115133af4ad6e94d75d914ca5a47c72012c4
Hexer X Nova <mrsports15.7.2024@gmail.com>
2026-01-15 13:47:38 +0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const sendSms = require('./send.js');
-
-const config = {
-	domain: '<your-domain-here>',
-	apiKey: '<your-api-key-here>'
-}
-
-sendSms(config,'<your-phone-number-here>', `hello world at ${new Date()}` ).then(result => console.log(result));
